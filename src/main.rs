507aa01f0f0208c267c3d7f71fc81c65c8c004d8
abdokin <bashbin.sh@gmail.com>
2023-11-15 01:04:52 +0100

    add list of functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,67 @@
+use std::f32::consts::PI;
+
+use functions::FunctionTable;
+use lazy_static::lazy_static;
+
+use crate::functions::Function;
+
+
 mod repl;
+mod functions;
 mod parser;
 
+
+lazy_static! {
+    static ref FUNCTION_TABLE: FunctionTable = {
+        let mut ft = FunctionTable::new();
+        ft.add_function("cos",
+            Function {
+                name: "cos".to_string(),
+                func: |a| a[0].cos(),
+                arg_count: 1,
+            },
+        );
+        ft.add_function("sin",
+            Function {
+                name: "sin".to_string(),
+                func: |a| a[0].sin(),
+                arg_count: 1,
+            },
+        );
+        ft.add_function("tan",
+            Function {
+                name: "tan".to_string(),
+                func: |a| a[0].tan(),
+                arg_count: 1,
+            },
+        );
+        ft.add_function("acos",
+            Function {
+                name: "acos".to_string(),
+                func: |a| a[0].acos(),
+                arg_count: 1,
+            },
+        );
+        ft.add_function("asin",
+            Function {
+                name: "asin".to_string(),
+                func: |a| a[0].asin(),
+                arg_count: 1,
+            },
+        );
+        ft.add_function("atan",
+            Function {
+                name: "atan".to_string(),
+                func: |a| a[0].atan(),
+                arg_count: 1,
+            },
+        );
+        ft
+    };
+}
+
+
 fn main() {
-    repl::Repl::start();    
+    repl::Repl::start(); 
+    // println!("{:#?}", *FUNCTION_TABLE);
 }
